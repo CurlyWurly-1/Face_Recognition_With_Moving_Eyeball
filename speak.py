3862097501a4d51ec64772c66dca6ab5de3cdd7f
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2025-02-01 13:53:02 +0000

    Update speak.py

--- speak.py
+++ speak.py
@@ -7,7 +7,7 @@ import pyttsx3
 
 # To install pyttsx3, use :
 #   sudo apt-get install espeak
-#   pip3 install pyttsx3 (not sure if you need to do this)
+#   pip install pyttsx3 (For windows)
 # 
 # For Gtts, the video says that you use
 #   sudo apt install mpg123
@@ -22,38 +22,48 @@ def print_hi(name):
     print(name)  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def say_hi_internet(name):
-#    text = 'Hi' + name + ', To say this, I need an internet connection'
-    text = name
+def say_hi_internet(text, filename="welcome.mp3"):
+    """Uses gTTS (Google Text-to-Speech) to say the given text.
+    Requires an internet connection.
+    """
     language = 'en'
     myobj = gTTS(text=text, lang=language, slow=False)
 
     try:
-        os.remove("welcome1.mp3")
-    except:
-        pass
-    myobj.save("welcome1.mp3")
-    playsound("welcome1.mp3")
-    os.remove("welcome1.mp3")
+        if os.path.exists(filename):
+            os.remove(filename)
+        myobj.save(filename)
+    #    os.system("play welcome1.mp3 tempo 1.2 >/dev/null 2>&1")  # If this doesn't work, comment out and use the playsound command as below
+        playsound(filename)           # This works for Win10/python 3.9
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
 
 
-def say_hi_local(name):
+def say_hi_local(text, voice_index=0, rate=150):
+    """Uses pyttsx3 to say the given text locally without requiring an internet connection."""
     engine = pyttsx3.init()
-    engine.setProperty('rate',150)
+    engine.setProperty('rate', rate)
+    voices = engine.getProperty('voices')
+    if voice_index < len(voices):
+        engine.setProperty('voice', voices[voice_index].id)
+
 #    engine.setProperty('voice','english+m1')
-    engine.setProperty('voice','english+f2')
-    engine.say('Hello' + name + ', to say this, I do NOT need an internet connection ')
+#    engine.setProperty('voice','english+f2')
+#    engine.say(name + ', to say this, I do NOT need an internet connection ')
+    engine.say(text)
     engine.runAndWait()
 
 
 def say_hi_local_different_voices(text):
+    """Uses pyttsx3 to say the given text in all available voices."""
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     for voice in voices:
-        print(voice.id)
+        print(f"Using voice: {voice.id}")
         engine.setProperty('voice', voice.id)  # changes the voice
         engine.say(text)
-    engine.runAndWait()
+        engine.runAndWait()
 
 
 # Press the green button in the gutter to run the script.
@@ -65,7 +75,13 @@ if __name__ == '__main__':
         text = "Hello world"
         pass
 
+    try:
+        voice_index = sys.argv[2]
+    except:
+        voice_index = 0
+        pass
+
 #    print_hi(text)
-    say_hi_internet(text)
-#    say_hi_local(text)
-#    say_hi_local_different_voices(text')
+#    say_hi_internet(text)
+    say_hi_local(text, voice_index)
+#    say_hi_local_different_voices(text)
